50a80757ed6a5602713a987d65c2b5f682861cfb
kribh03 <kribh03@ca.com>
2016-06-20 17:48:06 +0530

    Different Controls

--- app/components/textarea/TextAreaDirective.js
+++ app/components/textarea/TextAreaDirective.js
@@ -1,3 +1,29 @@
-/**
- * Created by kribh03 on 17-Jun-16.
- */
+(function (angular) {
+    'use strict';
+
+    angular.module('components').directive('caTextarea', function(formRenderHelper) {
+        return {
+            restrict: 'E',
+            templateUrl : 'Components/textarea/TextArea.html',
+            scope : {
+                data : '=',
+                load : '=',
+                parentFormId : '@'
+            },
+            link: function (scope, element, attrs) {
+                //scope.data;
+                //var jElement = $(element);
+                //var textArea = jElement.find('textarea')[0];
+                //scope.textFieldLabel = data["name"];
+                //var attrList = scope.data["attributeValues"];
+                //if(attrList.length > 0) {
+                //    for(var i in attrList){
+                //        textArea.attribute(formRenderHelper.getAttributeByAlias(attrList["name"]), attrList["value"]);
+                //    }
+                //}
+
+            }
+        }
+
+    });
+})(window.angular);
