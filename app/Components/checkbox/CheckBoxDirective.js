c00533af1279b77ea6bf298cfadf4c1ddc3c98e0
racsr02 <racsr02@ca.com>
2016-06-22 12:24:43 +0530

    textfield

--- app/Components/checkbox/CheckBoxDirective.js
+++ app/Components/checkbox/CheckBoxDirective.js
@@ -1,3 +1,37 @@
 /**
  * Created by kribh03 on 17-Jun-16.
  */
+
+(function (angular) {
+    'use strict';
+
+    angular.module('components').directive('caCheckbox', caCheckBoxDirective);
+
+    caCheckBoxDirective.$inject = ['catalog.formRenderHelper'];
+
+    function caCheckBoxDirective (formRenderHelper) {
+        return {
+            restrict: 'E',
+            templateUrl : 'Components/checkbox/CheckBox.html',
+            scope : {
+                data : '=',
+                rendererCfg : '=',
+                parentFormId : '@'
+            },
+            link: function (scope, element, attrs) {
+                var data = scope.data;
+                var jElement = $(element);
+                var cb = jElement.find('input');
+                scope.checkboxLabel = data["name"];
+                var attrList = data["attributeValues"];
+                if(attrList && attrList.length > 0) {
+                    for(var i in attrList){
+                        cb.attr(formRenderHelper.getAttributeByAlias(attrList[i]["name"]), attrList[i]["value"]);
+                    }
+                }
+
+            }
+        }
+
+    }
+})(window.angular);
--- app/Components/select/SelectDirective.js
+++ app/Components/select/SelectDirective.js
@@ -1,3 +1,36 @@
 /**
  * Created by kribh03 on 17-Jun-16.
  */
+(function (angular) {
+    'use strict';
+
+    angular.module('components').directive('caCombobox', caComboboxDirective);
+
+    caComboboxDirective.$inject = ['catalog.formRenderHelper'];
+
+    function caComboboxDirective (formRenderHelper) {
+        return {
+            restrict: 'E',
+            templateUrl : 'Components/select/Select.html',
+            scope : {
+                data : '=',
+                rendererCfg : '=',
+                parentFormId : '@'
+            },
+            link: function (scope, element, attrs) {
+                var data = scope.data;
+                var jElement = $(element);
+                var comboBox = jElement.find('select');
+                scope.comboboxLabel = data["name"];
+                var attrList = data["attributeValues"];
+                if(attrList && attrList.length > 0) {
+                    for(var i in attrList){
+                        comboBox.attr(attrList[i]["name"], attrList[i]["value"]);
+                    }
+                }
+
+            }
+        }
+
+    }
+})(window.angular);
